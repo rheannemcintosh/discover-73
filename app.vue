8f58beb1bac67f16f4cfe837e799e05c2b2cf17c
rheannemcintosh <rheannemcintosh@gmail.com>
2024-10-27 19:52:06 +0000

    Display the component
    [D73-17]

--- app.vue
+++ app.vue
@@ -1,4 +1,6 @@
 <template>
-    <ActivityGroups />
-    <CreateActivityGroup />
+    <div class="bg-gray-50 h-screen">
+        <Discover73Overview />
+        <CreateActivityGroup />
+    </div>
 </template>
