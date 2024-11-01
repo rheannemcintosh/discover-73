1c097460d7e3a06487dbc371f9176cdd1acec9c8
rheannemcintosh <rheannemcintosh@gmail.com>
2024-11-01 23:36:47 +0000

    Create an error message component
    [D73-27]

---
+++ components/ui/ErrorMessage.vue
@@ -0,0 +1,14 @@
+<!-- components/ErrorMessage.vue -->
+<template>
+    <div v-if="message" class="bg-red-600 rounded-md px-4 py-1 text-white text-sm">
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+const props = defineProps<{
+    message: string | null;
+}>();
+</script>
